7632febdae3896cc7a9bf178d1ba58e26185cd51
Archi1881 <detr1881@gmail.com>
2021-11-10 22:12:18 +0500

    урок 5 исправил в 5 раз

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  has_many :user_tests
   has_many :users, through: :user_tests
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
