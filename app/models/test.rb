c788ee0c6764cea8a88c548f84cac013c0bcda1e
Archi1881 <detr1881@gmail.com>
2021-11-12 23:16:15 +0500

    урок-6

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,15 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
+  validates :level, numericality: { only_integer: true}
+                    uniqueness: true
+  validates :title, presence: true
+                    uniqueness: true
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
   def self.desc_category_title(category_title)
   	joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: category_title}).order(title: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :tests, through: :user_tests
-  has_many :created_tests, class_name: "Test" 
+  has_many :created_tests, class_name: "Test"
+
+  validates :email, presence: true
+  
 	def  all_test(level_test)
     Test.joins("INNER JOIN users_test ON test.id = users_test.test_id").where(level: level_test, users_tests: {user_id: id})
   end
