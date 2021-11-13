443d0f992c362f0c95afa07da9a29ade3c0501be
Archi1881 <detr1881@gmail.com>
2021-11-13 22:13:24 +0500

    исправил урок-6

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,16 +5,18 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
-  validates :level, numericality: { only_integer: true}
-                    uniqueness: true
+  validates_uniqueness_of :title, scope: :level
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, presence: true
-                    uniqueness: true
 
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :level, ->(level) { where(level: level) }
+  scope :category, ->(c_title) { joins(:category)
+    .where(categories: {title: c_title}) }
 
-  def self.desc_category_title(category_title)
-  	joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: category_title}).order(title: :desc).pluck(:title)
+  def self.desc_category_title
+  	order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  has_many :user_tests
   has_many :tests, through: :user_tests
   has_many :created_tests, class_name: "Test"
 
   validates :email, presence: true
   
-	def  all_test(level_test)
-    Test.joins("INNER JOIN users_test ON test.id = users_test.test_id").where(level: level_test, users_tests: {user_id: id})
+	def user_tests(test)
+    user_tests.order(id: :desc).find_by(test: test)
   end
 end
