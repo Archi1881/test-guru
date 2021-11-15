e6dacff2c8b339b084fef6cd87da60a2c85c915c
Archi1881 <detr1881@gmail.com>
2021-11-15 20:04:51 +0500

    исправил 4й раз урок-6

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,18 +5,17 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
-  validates :title, scope: :level, uniqueness: true
+  validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :title, presence: true
-
+  
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :level, ->(level) { where(level: level) }
-  scope :category, ->(category_title) { joins(:category)
-    .where(categories: {title: category_title}).order(title: :desc) }
+  scope :by_level, ->(level) { where(level: level) }
+  scope :by_category_title, ->(category_title) { joins(:category)
+    .where(categories: {title: category_title}) }
 
-  def self.desc_category_title
-  	category.pluck(:title)
+  def self.desc_category_title(title)
+  	by_category_title(title).order(title: :desc).pluck(:title)
   end
 end
