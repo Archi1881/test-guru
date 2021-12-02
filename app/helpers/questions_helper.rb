e446531b9e33b5f3084e6370e71b670655095e76
Archi1881 <detr1881@gmail.com>
2021-12-02 23:43:49 +0500

    урок-8

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+    def question_header(question)
+      test_title = question.test.title
+    question.persisted? ? "Edit #{test_title} question" : "Create new question for test: #{test_title}"
+      end
 end
