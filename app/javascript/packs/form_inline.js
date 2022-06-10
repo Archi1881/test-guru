bb2d449f78b25e2a26e1e5301b5656dbd6bf3c98
Archi1881 <detr1881@gmail.com>
2022-06-11 00:13:14 +0500

    form_inline

--- app/javascript/packs/form_inline.js
+++ app/javascript/packs/form_inline.js
@@ -1,24 +1,26 @@
 document.addEventListener("turbolinks:load", function () {
-
+  
   let controls = document.querySelectorAll('.form-inline-link')
 
   if (controls.length) {
-    for (let i = 0; i < controls.length; i++) {
-        controls[i].addEventListener('click', formInlineLinkHandler)
-    }
+      for (let i = 0; i < controls.length; i++) {
+          controls[i].addEventListener('click', formInlineLinkHandler)
+      }
   }
 
+
+  //$(".form-inline-link").on("click", formInlineLinkHandler);
 
   let errors = document.querySelector(".resource-errors");
 
-  if (errors && controls.length) {
+  if (errors) {
       let resourceId = errors.dataset.resourceId;
       formInlineHandler(resourceId);
   }
-})
+});
 
 function formInlineLinkHandler(event) {
-  event.preventDefault();
+  event.preventDefault()
 
   let testId = this.dataset.testId;
   console.log(testId);
@@ -37,12 +39,12 @@ function formInlineHandler(testId) {
   //$testTitle.toggle();
 
   if (formInline.classList.contains('hide')) {
-    testTitle.classList.add('hide')
-    formInline.classList.remove('hide')
-    link.textContent = 'Cancel'
+      testTitle.classList.add('hide')
+      formInline.classList.remove('hide')
+      link.textContent = 'Cancel'
   } else {
-    testTitle.classList.remove('hide')
-    formInline.classList.add('hide')
-    link.textContent = 'Edit'
+      testTitle.classList.remove('hide')
+      formInline.classList.add('hide')
+      link.textContent = 'Edit'
   }
-}  
+}
